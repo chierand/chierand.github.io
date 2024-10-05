f254b20a74bfcabd4b64046459ae5d1c0f791bb1
Anmenig <35890296+anmenig@users.noreply.github.com>
2024-10-05 12:28:25 +0300

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,31 +1,6 @@
 const langArr = {
-    "unit" :  {
-        "en": "build a multi language site ",
-        "uk": "створюємо багатомовний сайт",
-    }, 
-    "chip": {
-        "en": "graphics processing unit",
-        "uk": "графічний чіп",
-    }, 
-    "memory": {
-        "en": "memory size",
-        "uk": "oбсяг пам'яті",
-    }, 
-    "memory-type": {
-        "en": "memory type",
-        "uk": "nип пам'яті",
-    }, 
-    "cool": {
-        "en": "сooling system type",
-        "uk": "тип системи охолодження",
-    }, 
-    "more": {
-        "en": "more details",
-        "uk": "детально про товар",
-    }, 
-
-   "js-head": {
-        "en": "<meta charset=UTF-8> <meta name=viewport content=width=device-width, initial-scale=1.0> <title lang=en>ItGid.info - 11111</title> <link rel=shortcut icon href=./favicon.ico type=image/x-icon> <link rel=stylesheet href=./style.css>",
-        "uk": "<meta charset=UTF-8> <meta name=viewport content=width=device-width, initial-scale=1.0> <title lang=ua>ItGid.info - 22222</title> <link rel=shortcut icon href=./favicon.ico type=image/x-icon> <link rel=stylesheet href=./style.css>",    
+     "js-head": {
+        "uk": "<meta charset=UTF-8> <meta name=viewport content=width=device-width, initial-scale=1.0> <title lang=en>ItGid.info - 11111</title> <link rel=shortcut icon href=./favicon.ico type=image/x-icon> <link rel=stylesheet href=./style.css>",
+        "en": "<meta charset=UTF-8> <meta name=viewport content=width=device-width, initial-scale=1.0> <title lang=ua>ItGid.info - 22222</title> <link rel=shortcut icon href=./favicon.ico type=image/x-icon> <link rel=stylesheet href=./style.css>",    
     }, 
 }
